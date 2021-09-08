1535ad38071cab10c207291a2f098df7683c25b1
CaotiHazard <guilsalomao@gmail.com>
2021-09-08 10:32:06 -0300

    Criada atividade pagina de usuario

---
+++ Android/App/app/src/main/java/br/ufscar/dc/mobile/app/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package br.ufscar.dc.mobile.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
