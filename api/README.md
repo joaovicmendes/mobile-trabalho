58b85e04bb29c8afa0c6557d3cdcba9692051787
João Victor Mendes <mendesjoaovic@gmail.com>
2021-09-12 18:26:26 -0300

    Mais detalhes da API adicionados

---
+++ api/README.md
@@ -0,0 +1,38 @@
+# API de Cursos
+
+## Courses
+Payload para adicionar um novo curso:
+
+```json
+{
+    "title": "Título",
+    "instructorId": "eO4pC6odTu00TT2AczJJ",
+    "description": "Descrição",
+    "rating": 4,
+    "categoryId": "KWc45Hp0VgGaTqdD1Fb4",
+    "schedule": "none",
+    "videoUrl": "none",
+    "thumbnail": "none"
+}
+```
+
+## Users
+Payload para adicionar um novo usuário:
+
+```json
+{
+    "name": "Nome do Usuário",
+    "username": "user_name",
+    "email": "email@email.com",
+}
+```
+
+## Category
+Payload para adicionar uma nova categoria:
+
+```json
+{
+    "name": "Nome da Categoria",
+    "hexColor": "#FFFFFF"
+}
+```
--- api/functions/index.js
+++ api/functions/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const app = express();
 admin.initializeApp();
 
+// Courses API
 app.get('/courses', async(req, res) => {
     const snapshot = await admin.firestore().collection('courses').get();
     let courses = [];
@@ -37,6 +38,76 @@ app.delete('/courses/:id', async (req, res) => {
     courseId = snapshot.id;
     courseData = snapshot.data();
     res.status(200).send(JSON.stringify({courseId, ...courseData}));
+});
+
+
+// Users API
+app.get('/users', async(req, res) => {
+    const snapshot = await admin.firestore().collection('users').get();
+    let users = [];
+    snapshot.forEach(doc => {
+        let id = doc.id;
+        let data = doc.data();
+        users.push({id, ...data});
+    });
+    res.status(200).send(JSON.stringify(users));
+});
+
+app.get('/users/:id', async (req, res) => {
+    const snapshot = await admin.firestore().collection('users').doc(req.params.id).get();
+    userId = snapshot.id;
+    userData = snapshot.data();
+    res.status(200).send(JSON.stringify({userId, ...userData}));
+});
+
+app.post('/users', async (req, res) => {
+    const user = req.body;
+    await admin.firestore().collection('users').add(user);
+    res.status(201).send(JSON.stringify(user));
+});
+
+app.delete('/users/:id', async (req, res) => {
+    const snapshot = await admin.firestore().collection('users').doc(req.params.id).get();
+    await admin.firestore().collection('users').doc(req.params.id).delete();
+
+    userId = snapshot.id;
+    userData = snapshot.data();
+    res.status(200).send(JSON.stringify({userId, ...userData}));
+});
+
+
+// Category API
+app.get('/categories', async(req, res) => {
+    const snapshot = await admin.firestore().collection('categories').get();
+    let categories = [];
+    snapshot.forEach(doc => {
+        let id = doc.id;
+        let data = doc.data();
+        categories.push({id, ...data});
+    });
+    res.status(200).send(JSON.stringify(categories));
+});
+
+app.get('/categories/:id', async (req, res) => {
+    const snapshot = await admin.firestore().collection('categories').doc(req.params.id).get();
+    categoryId = snapshot.id;
+    categoryData = snapshot.data();
+    res.status(200).send(JSON.stringify({categoryId, ...categoryData}));
+});
+
+app.post('/categories', async (req, res) => {
+    const category = req.body;
+    await admin.firestore().collection('categories').add(category);
+    res.status(201).send(JSON.stringify(category));
+});
+
+app.delete('/categories/:id', async (req, res) => {
+    const snapshot = await admin.firestore().collection('categories').doc(req.params.id).get();
+    await admin.firestore().collection('categories').doc(req.params.id).delete();
+
+    categoryId = snapshot.id;
+    categoryData = snapshot.data();
+    res.status(200).send(JSON.stringify({categoryId, ...categoryData}));
 });
 
 exports.api = functions.https.onRequest(app);
